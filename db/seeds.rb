99daf64fd82e8f66d667c641b6b2d6a451b41d44
zdurr22 <zdurr22@gmail.com>
2021-08-06 14:56:15 -0400

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+categories = Category.create([{ name: 'First Category'}, { name: 'Second Category'}])
